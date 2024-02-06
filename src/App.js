6a63ad704ed4ea0416ec5e5635540d02600ef3ab
nivashinib66 <154663593+nivashinib66@users.noreply.github.com>
2024-02-06 19:43:30 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>Todo List</h2>
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
